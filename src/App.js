e0559354f365715088de6289c47952c0192c505b
stillfighting <2522910086@qq.com>
2023-11-07 10:33:59 -0500

    d

--- src/App.js
+++ src/App.js
@@ -22,17 +22,15 @@ function App() {
 
 	return (
 		<div className="App">
-			<BrowserRouter basename="/web">
-				<Routes>
-					<Route path="/" element={<Homepage />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/projects" element={<Projects />} />
-					<Route path="/articles" element={<Articles />} />
-					<Route path="/article/:slug" element={<ReadArticle />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="*" element={<Notfound />} />
-				</Routes>
-			</BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/articles" element={<Articles />} />
+				<Route path="/article/:slug" element={<ReadArticle />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="*" element={<Notfound />} />
+			</Routes>
 		</div>
 	);
 }
